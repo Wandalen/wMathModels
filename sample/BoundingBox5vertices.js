d6c8d768a16c2bf7f96a1a97be377a4d158923a7
PabloGarcimartin <pablogarcimartinruiz@gmail.com>
2018-06-06 17:27:37 +0200

    Box vertices changed to random vectors instead of hardcoded values

--- sample/BoundingBox5vertices.js
+++ sample/BoundingBox5vertices.js
@@ -3,28 +3,28 @@ require( 'wmathconcepts' );
 
 var _ = wTools;
 
-var point1 = [1,0,0]
+var point1 = _.avector.arrayMakeRandom(3);
 console.log( 'points :',point1 );
 var src = null;
  var got = _.box.pointExpand( src , point1);
 console.log( 'box centered :',got );
 
-var point2 = [0,1,0]
+var point2 = _.avector.arrayMakeRandom(3);
 console.log( 'points :',point2 );
  var got = _.box.pointExpand( got , point2);
 console.log( 'box centered :',got );
 
-var point3 = [0,0,1]
+var point3 = _.avector.arrayMakeRandom(3);
 console.log( 'points :',point3 );
  var got = _.box.pointExpand( got , point3);
 console.log( 'box centered :',got );
 
-var point4 = [-1,-1,-1]
+var point4 = _.avector.arrayMakeRandom(3);
 console.log( 'points :',point4 );
  var got = _.box.pointExpand( got , point4);
 console.log( 'box centered :',got );
 
-var point5 = [2,-2,0]
+var point5 = _.avector.arrayMakeRandom(3);
 console.log( 'points :',point5 );
  var got = _.box.pointExpand( got , point5);
 console.log( 'box centered :',got );
