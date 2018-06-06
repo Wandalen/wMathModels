d26f2f9350f98689d51329a6c2f742fc787d30f6
Wandalen <wandalen.me@gmail.com>
2018-06-06 10:55:57 +0300

    sync

--- sample/SampleBoxCentered.js
+++ sample/SampleBoxCentered.js
@@ -6,8 +6,6 @@ var _ = wTools;
 
 var src = undefined;
 var got = _.box.centeredOfSize( src );
-var expected = [ -0.5,-0.5,-0.5,+0.5,+0.5,+0.5 ];
-//test.identical( got,expected );
-//test.shouldBe( got !== src );
-console.log( 'box centered expected: ',expected );
-console.log( 'box centered got: ',got );
+console.log( 'box centered :',got );
+
+// var expected = [ -0.5,-0.5,-0.5,+0.5,+0.5,+0.5 ];
