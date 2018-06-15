5d689c7912ebe62f8739cb611272fb580a4f994b
PabloGarcimartin <pablogarcimartinruiz@gmail.com>
2018-06-15 11:59:44 +0200

    sphereDistance experiment.

--- sample/SamplePlane.js
+++ sample/SamplePlane.js
@@ -15,18 +15,3 @@ console.log('SphereDistance: ',dist);
 // console.log('Array: ',sphere,' - Vector: ', vsphere );
 
 debugger;
-
-
-console.log('Coplanar **************************');
-
-var plane = [ 1, 0, 0, 0 ];
-var vplane = _.vector.from(plane);
-pointa = [ 5, 4, 4 ];
-var vpointa = _.vector.from(pointa);
-console.log('Point: ',pointa);
-//var coplanar = _.plane.pointCoplanarGet( plane, pointa );
-var coplanar = _.plane.pointCoplanarGet( vplane, vpointa );
-//console.log('pointCoplanarGet: ',coplanar);
-console.log('Plane: ',plane);
-console.log('Point: ',pointa);
-console.log('pointCoplanarGet: ',coplanar);
