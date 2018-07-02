47e1398c2e1f27c37a6ccbcff0e964583b45032b
PabloGarcimartin <pablogarcimartinruiz@gmail.com>
2018-07-02 08:17:32 +0200

    Euler: toMatrix2 test routine in progress.

--- sample/SampleEuler.js
+++ sample/SampleEuler.js
@@ -22,12 +22,14 @@ var dstYZY = [ 0, 0, 0, 1, 2, 1 ];
 var dstZXZ = [ 0, 0, 0, 2, 0, 2 ];
 var dstZYZ = [ 0, 0, 0, 2, 1, 2 ];
 
-var euler = [ - 0.2, 0, 0, 2, 0, 2 ];
-var mat = _.euler.toMatrix2( euler );
-var euler2 = _.euler.fromMatrix2( mat, dstZXZ );
-console.log( 'Euler from mat xyz:', euler2 );
 
-var euler = [ - 0.2, Math.PI, 0, 2, 0, 2 ];
-var mat = _.euler.toMatrix2( euler );
-var euler2 = _.euler.fromMatrix2( mat, dstZXZ );
-console.log( 'Euler from mat xyz:', euler2 );
+var euler = [ 0.5, 0.5, 0.5, 0, 1, 2 ];
+var expected = _.Space.make( [ 3, 3 ] ).copy(
+       [ 0.7701, -0.4207, 0.4794,
+         0.6224, 0.6599, - 0.4207,
+         - 0.1393, 0.6224, 0.7701 ] );
+
+var result = _.euler.toMatrix2( euler );
+console.log( 'Euler from mat xyz:', result );
+var result = _.euler.fromMatrix2( result, dstXYZ );
+console.log( 'Euler from mat xyz:', result );
