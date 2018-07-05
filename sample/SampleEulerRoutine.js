a9fdadb9040fa7148a8ce268c934a45aaff3b1a1
PabloGarcimartin <pablogarcimartinruiz@gmail.com>
2018-07-05 15:11:51 +0200

    Sample for eulerToQuatToEulerToQuat.

--- sample/SampleEulerRoutine.js
+++ sample/SampleEulerRoutine.js
@@ -1,5 +1,6 @@
 if( typeof module !== 'undefined' )
 require( 'wmathconcepts' );
+require( 'wTesting' );
 
 var _ = wTools;
 
@@ -8,12 +9,35 @@ var Angle = [ 0, Math.PI / 6, Math.PI / 4, Math.PI / 3 ];
 var Quadrant = [ 0, 1, 2 ];
 var accuracy =  1e-7;
 var Delta = [ -0.1, -Math.sqrt( accuracy ), -( accuracy*accuracy ), 0, +( accuracy*accuracy ), +Math.sqrt( accuracy ), +0.1 ];
+var euler = [ 0, 0, 0, 0, 0, 0 ];
 
-var T = 0;
-var F = 0;
+function onEach( euler)
+{
+  var dstEuler = euler.slice();
+  dstEuler[ 0 ] = 0;
+  dstEuler[ 1 ] = 0;
+  dstEuler[ 2 ] = 0;
+  var expected = _.euler.toQuat2( euler );
+  var euler2 = _.euler.fromQuat2( expected, dstEuler );
+  var result = _.euler.toQuat2( euler2 );
 
-debugger;
-function onEach( euler, eulerEmpty )
+  var expected = _.vector.toArray( expected );
+  var positiveResult = _.vector.toArray( result );
+  var negativeResult = _.avector.mul( _.vector.toArray( result ), -1 );
+  var eq1 = _.entityEquivalent( positiveResult, expected);
+  var eq2 = _.entityEquivalent( negativeResult, expected );
+  if ( eq1 === 'true' || eq2 === 'true')
+  {
+    console.log(eq1,eq2);
+  }
+  else
+  {
+    console.log('FALSE');
+  }
+  //test.is( eq1 || eq2 );
+}
+
+function onEach1( euler )
 {
   var dstEuler = euler.slice();
   dstEuler[ 0 ] = 0;
@@ -32,43 +56,20 @@ function onEach( euler, eulerEmpty )
   var negDif3 = Math.abs( expected.eGet( 3 ) +  result.eGet( 3 ) );
 
   if( posDif0 < accuracy && posDif1 < accuracy && posDif2 < accuracy && posDif3 < accuracy )
-  { T = T+1; }
+  { console.log('TRUE'); }
   else if( negDif0 < accuracy && negDif1 < accuracy && negDif2 < accuracy && negDif3 < accuracy )
-  { T = T+1; }
+  { console.log('TRUE'); }
   else
-  { F = F +1; }
+  { console.log('FALSE'); }
 }
 
+euler[ 0 ] = Angle[ 1 ] + Quadrant[ 0 ]*Math.PI/2 + Delta[ 1 ];
+euler[ 1 ] = Angle[ 3 ] + Quadrant[ 1 ]*Math.PI/2 + Delta[ 3 ];
+euler[ 2 ] = Angle[ 3 ] + Quadrant[ 1 ]*Math.PI/2 + Delta[ 3 ];
+euler[ 3 ] = 0;
+euler[ 4 ] = 1;
+euler[ 5 ] = 2;
 
-for( var i = 0; i < EulerSeqs.length; i++ )
-{
-  var seq = EulerSeqs[ i ];
-  var euler = [ 0, 0, 0, 0, 0, 0 ];
-  euler = _.euler.make2( euler, seq );
-  for( var ang = 0; ang < Angle.length; ang++ )
-  {
-    for( var quad = 0; quad < Quadrant.length; quad++ )
-    {
-      for( var d = 0; d < Delta.length; d++ )
-      {
-        euler[ 0 ] = Angle[ ang ] + Quadrant[ quad ]*Math.PI/2 + Delta[ d ];
-        for( var ang2 = 0; ang2 < Angle.length; ang2++ )
-        {
-          for( var quad2 = 0; quad2 < Quadrant.length; quad2++ )
-          {
-            for( var d2 = 0; d2 < Delta.length; d2++ )
-            {
-              euler[ 1 ] = Angle[ ang2 ] + Quadrant[ quad2 ]*Math.PI/2 + Delta[ d2 ];
-              euler[ 2 ] = Angle[ ang2 ] + Quadrant[ quad2 ]*Math.PI/2 + Delta[ d2 ];
-              var eulerEmpty = _.euler.make2( null, seq );
-              onEach( euler );
-            }
-          }
-        }
-      }
-    }
-  }
-}
+onEach( euler );
 
-console.log( 'T = ', T);
-console.log( 'F = ', F);
+onEach1( euler );
