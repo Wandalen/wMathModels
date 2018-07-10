0056b2d454be985bdd62b5d60cfe0aed0463eb88
Wandalen <wandalen.me@gmail.com>
2018-07-10 10:44:19 +0300

    sync

--- staging/dwtools/amath/geometric.test/Euler.test.s
+++ staging/dwtools/amath/geometric.test/Euler.test.s
@@ -2464,10 +2464,24 @@ var Self =
 
 /*
 
+= Common for all axis
+
 Euler representations = [ 'xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx', 'xyx', 'xzx', 'yxy', 'yzy', 'zxz', 'zyz' ]
-Angle = set[ 0, PI / 6, PI / 4, PI / 3 ]
 Accuracy = epsilon, by default it is 1e-7
+
+= Ordinary axis
+
+Angle = set[ 0, PI / 6, PI / 4, PI / 3 ]
 Delta = set[ -0.1, -sqrt( Accuracy ), -sqr( Accuracy ), 0, +sqr( Accuracy ), +sqrt( Accuracy ), +0.1 ]
+Quadrant = [ 0, 1, 2, 3 ]
+
+= Reduced axis
+
+Angle = set[ 0, PI / 6, PI / 4, PI / 3 ]
+Delta = set[ 0 ]
+Quadrant = [ 0 ]
+
+= Total number of test cases
 
                       Angle + n*PI / 2 + Delta
         0.5 *       (  4   *   4      *   7   )     ^ 3
