dcaf638b18fe08ece4756c45425ed10f567ee4f2
Pablo <pablogarcimartinruiz@gmail.com>
2018-07-16 15:49:29 +0200

    _.box.from() test routine error samole.

--- sample/SampleEuler.js
+++ sample/SampleEuler.js
@@ -7,6 +7,8 @@ var numXYZ = [ 0, 1, 2 ];
 var numXZY = [ 0, 2, 1 ];
 var txtXYZ = 'xyz';
 var txtYZY = 'yzy';
+var dstXYZ = [ 0, 0, 0, 0, 1, 2 ];
+var dstXZY = [ 0, 0, 0, 0, 2, 1 ];
 var dstYXZ = [ 0, 0, 0, 1, 0, 2 ];
 var dstYZX = [ 0, 0, 0, 1, 2, 0 ];
 var dstZXY = [ 0, 0, 0, 2, 0, 1 ];
@@ -20,11 +22,11 @@ var dstZYZ = [ 0, 0, 0, 2, 1, 2 ];
 
 debugger;
 
-var dstEulerN = null;
-var dstEuler = [ 1, 1, 1, 1, 0, 2 ];;
+var euler =[ 1, 0.25, 2, 1, 2, 0 ];
 //var resN = _.euler.represent( dstEulerN, txtYZY );
-var res = _.euler.toQuat2( srcEuler, numXZY );
+var res = _.euler.toQuat2( euler, null );
+console.log( 'Old Euler:', res);
 
-//console.log( 'Old Euler:', resN);
-//console.log( 'Old Euler:', dstEulerN);
+var quat = [ 0.3252921697349392, 0.39636481102592414, 0.09544332266900905, 0.8532118805123485];
+res = _.euler.fromQuat2( dstYXZ, quat );
 console.log( 'Old Euler:', res);
---
+++ sample/SampleFromBox.js
@@ -0,0 +1,11 @@
+
+if( typeof module !== 'undefined' )
+require( 'wmathconcepts' );
+
+var _ = wTools;
+
+ var box = _.vector.fromArray( [ 0, 0, 2, 2 ] );
+ var box = _.box.from( box );
+
+ console.log( box );
+debugger;
