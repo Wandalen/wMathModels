e8cf3d3f7880814cbfb12af906149fbd288196f2
Pablo <pablogarcimartinruiz@gmail.com>
2018-08-08 12:50:20 +0200

    _.ray.diomGet, _.ray.originGet and _.ray.directionGet testroutines

--- staging/dwtools/amath/geometric.test/Ray.test.s
+++ staging/dwtools/amath/geometric.test/Ray.test.s
@@ -68,7 +68,7 @@ function make( test )
   var srcDim = null;
   var gotRay = _.ray.make( srcDim );
   var expected = [ 0, 0, 0, 0, 0, 0 ];
-  test.identical( gotRay,expected );
+  test.identical( gotRay, expected );
   test.is( gotRay !== srcDim );
 
   test.case = 'srcDim 2'; /* */
@@ -76,7 +76,7 @@ function make( test )
   var srcDim = 2;
   var gotRay = _.ray.make( srcDim );
   var expected = [ 0, 0, 0, 0 ];
-  test.identical( gotRay,expected );
+  test.identical( gotRay, expected );
   test.is( gotRay !== srcDim );
 
   test.case = 'srcDim array'; /* */
@@ -84,7 +84,7 @@ function make( test )
   var srcDim = [ 0, 1, 2, 3 ];
   var gotRay = _.ray.make( srcDim );
   var expected = [ 0, 1, 2, 3 ];
-  test.identical( gotRay,expected );
+  test.identical( gotRay, expected );
   test.is( gotRay !== srcDim );
 
   test.case = 'srcDim vector'; /* */
@@ -92,7 +92,7 @@ function make( test )
   var srcDim = _.vector.fromArray([ 0, 1, 2, 3 ]);
   var gotRay = _.ray.make( srcDim );
   var expected = [ 0,1,2,3 ];
-  test.identical( gotRay,expected );
+  test.identical( gotRay, expected );
   test.is( gotRay !== srcDim );
 
   /* */
@@ -112,16 +112,16 @@ function is( test )
   test.case = 'array'; /* */
 
   test.is( _.ray.is( [] ) );
-  test.is( _.ray.is([ 0,0 ]) );
-  test.is( _.ray.is([ 1,2,3,4 ]) );
-  test.is( _.ray.is([ 0,0,0,0,0,0 ]) );
+  test.is( _.ray.is([ 0, 0 ]) );
+  test.is( _.ray.is([ 1, 2, 3, 4 ]) );
+  test.is( _.ray.is([ 0, 0, 0, 0, 0, 0 ]) );
 
   test.case = 'vector'; /* */
 
   test.is( _.ray.is( _.vector.fromArray([]) ) );
-  test.is( _.ray.is( _.vector.fromArray([ 0,0 ]) ) );
-  test.is( _.ray.is( _.vector.fromArray([ 1,2,3,4 ]) ) );
-  test.is( _.ray.is( _.vector.fromArray([ 0,0,0,0,0,0 ]) ) );
+  test.is( _.ray.is( _.vector.fromArray([ 0, 0 ]) ) );
+  test.is( _.ray.is( _.vector.fromArray([ 1, 2, 3, 4 ]) ) );
+  test.is( _.ray.is( _.vector.fromArray([ 0, 0, 0, 0, 0, 0 ]) ) );
 
   test.case = 'not ray'; /* */
 
@@ -148,6 +148,224 @@ function is( test )
 }
 
 //
+
+function dimGet( test )
+{
+  test.case = 'srcRay 1D - array'; /* */
+
+  var srcRay = [ 0, 1 ];
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 1;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  test.case = 'srcRay 1D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1 ] );
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 1;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  test.case = 'srcRay 2D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3 ];
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 2;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  test.case = 'srcRay 2D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3 ] );
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 2;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  test.case = 'srcRay 3D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3, 4, 5 ];
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 3;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  test.case = 'srcRay 3D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3, 4, 5 ] );
+  var gotDim = _.ray.dimGet( srcRay );
+  var expected = 3;
+  test.identical( gotDim, expected );
+  test.is( gotDim !== srcRay );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+  test.shouldThrowErrorSync( () => _.ray.dimGet( ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( [ 0, 0, 0 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( [ 0, 0 ], [ 1, 1 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( 'ray' ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( 0 ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( null ) );
+  test.shouldThrowErrorSync( () => _.ray.dimGet( undefined ) );
+
+}
+
+//
+
+function originGet( test )
+{
+  test.case = 'Source ray remains unchanged'; /* */
+
+  var srcRay = [ 0, 0, 1, 1 ];
+  var expected = _.vector.from( [ 0, 0 ] );
+
+  var gotOrigin = _.ray.originGet( srcRay );
+  test.identical( gotOrigin, expected );
+
+  var oldSrcRay = [ 0, 0, 1, 1 ];
+  test.equivalent( srcRay, oldSrcRay );
+
+  test.case = 'srcRay 1D - array'; /* */
+
+  var srcRay = [ 0, 1 ];
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  test.case = 'srcRay 1D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1 ] );
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  test.case = 'srcRay 2D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3 ];
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0, 1 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  test.case = 'srcRay 2D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3 ] );
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0, 1 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  test.case = 'srcRay 3D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3, 4, 5 ];
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0, 1, 2 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  test.case = 'srcRay 3D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3, 4, 5 ] );
+  var gotOrigin = _.ray.originGet( srcRay );
+  var expected = _.vector.from( [ 0, 1, 2 ] );
+  test.identical( gotOrigin, expected );
+  test.is( gotOrigin !== srcRay );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+  test.shouldThrowErrorSync( () => _.ray.originGet( ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( [ 0, 0, 0 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( [ 0, 0 ], [ 1, 1 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( 'ray' ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( 0 ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( null ) );
+  test.shouldThrowErrorSync( () => _.ray.originGet( undefined ) );
+
+}
+
+//
+
+function directionGet( test )
+{
+  test.case = 'Source ray remains unchanged'; /* */
+
+  var srcRay = [ 0, 0, 1, 1 ];
+  var expected = _.vector.from( [ 1, 1 ] );
+
+  var gotDirection = _.ray.directionGet( srcRay );
+  test.identical( gotDirection, expected );
+
+  var oldSrcRay = [ 0, 0, 1, 1 ];
+  test.equivalent( srcRay, oldSrcRay );
+
+  test.case = 'srcRay 1D - array'; /* */
+
+  var srcRay = [ 0, 1 ];
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 1 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  test.case = 'srcRay 1D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1 ] );
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 1 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  test.case = 'srcRay 2D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3 ];
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 2, 3 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  test.case = 'srcRay 2D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3 ] );
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 2, 3 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  test.case = 'srcRay 3D - array'; /* */
+
+  var srcRay = [ 0, 1, 2, 3, 4, 5 ];
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 3, 4, 5 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  test.case = 'srcRay 3D - vector'; /* */
+
+  var srcRay = _.vector.fromArray( [ 0, 1, 2, 3, 4, 5 ] );
+  var gotDirection = _.ray.directionGet( srcRay );
+  var expected = _.vector.from( [ 3, 4, 5 ] );
+  test.identical( gotDirection, expected );
+  test.is( gotDirection !== srcRay );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+  test.shouldThrowErrorSync( () => _.ray.directionGet( ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( [ 0, 0, 0 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( [ 0, 0 ], [ 1, 1 ] ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( 'ray' ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( 0 ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( null ) );
+  test.shouldThrowErrorSync( () => _.ray.directionGet( undefined ) );
+
+}
 
 //
 
@@ -182,6 +400,10 @@ var Self =
     make : make,
 
     is : is,
+
+    dimGet : dimGet,
+    originGet : originGet,
+    directionGet : directionGet,
 
     fromPoints : fromPoints,
 
