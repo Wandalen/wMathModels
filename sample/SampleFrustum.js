6d3b489ea3f13cc9078da6163a4891904d34b1bb
Pablo <pablogarcimartinruiz@gmail.com>
2018-08-08 09:13:52 +0200

    Frustum.s corrections

--- sample/SampleFrustum.js
+++ sample/SampleFrustum.js
@@ -10,11 +10,7 @@ var srcFrustum= _.Space.make( [ 4, 6 ] ).copy
   0,   0,   1, - 1,   0,   0,
   - 1,   0, - 1,   0,   0, - 1
 ]);
-var box = [ 3, 3, 3, 4, 4, 4 ];
+var box = [ 3, 3, 3 ];
 
-var result = _.frustum.boxDistance( srcFrustum, box )
+var result = _.frustum.pointClosestPoint( srcFrustum, box )
 console.log( 'Result: ', result );
-
-var result = _.box.frustumDistance( box, srcFrustum )
-console.log( 'Result: ', result );
-debugger;
