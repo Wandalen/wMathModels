9eb19a4e2ae4b735b4223af30b5f9b51e2c3732a
vmigdal <volodymyr.migdal@gmail.com>
2020-03-24 10:48:30 +0200

    ConcavePolygon: replace pointContains

--- proto/dwtools/amath/l7_concept/ConcavePolygon.s
+++ proto/dwtools/amath/l7_concept/ConcavePolygon.s
@@ -140,75 +140,49 @@ function is( polygon )
 function pointContains( polygon, point )
 {
   let self = this;
-  //let c = [ point[ 0 ], point[ 1 ], 1 ];
-  let line21 = [];
-  let line20 = [];
+
   let p = 0;
   let pl = polygon.length / 2;
-  let inside = 0;
 
   _.assert( pl === 2, 'not implemented' );
 
-  //
-
-  function pointsPointSide( points, point )
-  {
-
-    let point1 = [];
-    point1[ 0 ] = points[ 0 ][ 0 ] - point[ 0 ];
-    point1[ 1 ] = points[ 0 ][ 1 ] - point[ 1 ];
-
-    let point2 = [];
-    point2[ 0 ] = points[ 1 ][ 0 ] - point[ 0 ];
-    point2[ 1 ] = points[ 1 ][ 1 ] - point[ 1 ];
-
-    if( point1[ 0 ] < 0 && point2[ 0 ] < 0 )
-    return false;
-
-    if( point1[ 0 ] > 0 && point2[ 0 ] > 0 )
-    return false;
-
-    if( point1[ 0 ] === point2[ 0 ] )
-    {
-      if( point1[ 1 ] < 0 && point2[ 1 ] < 0 )
-      return false;
-
-      if( point1[ 1 ] > 0 && point2[ 1 ] > 0 )
-      return false;
-
-      return 2;
-    }
-
-    let upper = point2[ 1 ] - point2[ 0 ] * ( point1[ 1 ]-point2[ 1 ] ) / ( point1[ 0 ]-point2[ 0 ] );
-
-    if( upper === 0 )
-    return 2;
-
-    return upper >= 0;
-  }
-
-  //
-
   let p1 = [ polygon[ (pl-1)*2+0 ], polygon[ (pl-1)*2+1 ] ];
-  let p2 = [ polygon[ (p+0)*2+0 ], polygon[ (p+0)*2+1 ] ];
-  let side = pointsPointSide( [ p1, p2 ], point );
-  if( side === 2 ) return 1;
-  inside = inside + side;
-
-  //
 
   for( p = 1 ; p < pl ; p++ )
   {
 
-    let p1 = [ polygon[ (p-1)*2+0 ], polygon[ (p-1)*2+1 ] ];
-    let p2 = [ polygon[ (p+0)*2+0 ], polygon[ (p+0)*2+1 ] ];
-    let side = pointsPointSide( [ p1, p2 ], point );
-    if( side === 2 ) return p+1;
-    inside = inside + side;
+    let p2 = [ polygon[ (p-1)*2+0 ], polygon[ (p-1)*2+1 ] ];
+    let p3 = [ polygon[ (p+0)*2+0 ], polygon[ (p+0)*2+1 ] ];
 
+    let side = this.tools.line.pointsToPointSide( [ p1, p2 ], point );
+    if( side === 0 )
+    {
+      let r = this.tools.segment.relativeSegment( [ p1, p2 ], point );
+      return 0 <= r && r <= 1 ? p : 0;
+    }
+
+    let cside1 = this.tools.line.pointsToPointSide( [ p2, p3 ], point );
+    if( side*cside1 < 0 )
+    continue;
+    else if( cside1 === 0 )
+    {
+      let r = this.tools.segment.relativeSegment( [ p2, p3 ], point );
+      return 0 <= r && r <= 1 ? p : 0;
+    }
+
+    let cside2 = this.tools.line.pointsToPointSide( [ p3, p1 ], point );
+    if( side*cside2 < 0 )
+    continue;
+    else if( cside2 === 0 )
+    {
+      let r = this.tools.segment.relativeSegment( [ p3, p1 ], point );
+      return 0 <= r && r <= 1 ? p : 0;
+    }
+
+    return pl+1;
   }
 
-  return inside % 2 ? pl+1 : 0;
+  return 0;
 }
 
 //
