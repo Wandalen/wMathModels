bdecd547149d196c4a2453fc97b8d35caf79950a
vmigdal <volodymyr.migdal@gmail.com>
2020-03-24 12:09:11 +0200

    concavePolygon: adjust isClockwise

--- proto/dwtools/amath/l8.test/ConcavePolygon.test.s
+++ proto/dwtools/amath/l8.test/ConcavePolygon.test.s
@@ -454,7 +454,7 @@ function isClockwise( test )
   test.case = 'concave counter clockwise'
 
   var polygon =
-  _.convexPolygon.make( 2, 4 ).copy
+  _.concavePolygon.make( 2, 4 ).copy
   ([
     6.84, 1.26, 2.32, 5.46,
     0.64, 1.54, 4.71, 4.93,
@@ -462,12 +462,13 @@ function isClockwise( test )
   test.is( !_.concavePolygon.isClockwise( polygon ) );
 
   test.case = 'concave clockwise'
-  var polygon = _.convexPolygon.make( 2, 4 ).copy
+  var polygon = _.concavePolygon.make( 2, 4 ).copy
   ([
     5.46,2.32,1.26,6.84,
     4.93,4.71,1.54,0.64
   ])
   test.is( _.concavePolygon.isClockwise( polygon ) );
+
 }
 
 // --
