cca1f85d2c0c05d33274efb8ce7742d555e1330c
Dmytro <dm.vict.kr@gmail.com>
2020-06-24 13:50:15 +0300

    Add directory `introduction2` with samples to file `Introduction.md`

---
+++ sample/introduction2/5_BoxFromBox.js
@@ -0,0 +1,15 @@
+if( typeof module !== 'undefined' )
+require( 'wmathmodels' );
+
+var _ = wTools;
+
+/* */
+
+var srcBox = new F32x([ 0, 1, 2, 3 ]);
+var box = _.box.from( srcBox );
+console.log( 'srcBox === box :', srcBox === box );
+/* log : srcBox === box : true */
+console.log( 'Type :', _.strType( box ) );
+/* log : Type : F32x */
+console.log( box );
+/* log : Float32Array(4) [ 0, 1, 2, 3 ] */
