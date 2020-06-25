13d158ce1b53b818ef96993cf8885ae2046ed588
Dmytro <dm.vict.kr@gmail.com>
2020-06-25 18:19:00 +0300

    Fix mistakes

--- sample/overview/7_LinePoints.js
+++ sample/overview/7_LinePoints.js
@@ -9,4 +9,4 @@ var line = [ 2, 1, 4, 2 ];
 var point = [ 2, 3 ];
 var distance = _.linePoints.pointDistance( line, point );
 console.log( `Distance from line to point : ${ _.toStr( distance, { precision : 2 } ) }` );
-/* log : Distance from line to point : 1.7 */
+/* log : Distance from line to point : 1.8 */
